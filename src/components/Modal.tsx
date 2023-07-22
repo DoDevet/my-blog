ed0f31a353dfc8512812e9e37456945ed8e32fa1
Telescope <ehwlgns000@gmail.com>
2023-07-22 17:06:32 +0900

    refactor: refactor modal Navigation

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,26 +1,27 @@
+"use client";
+import { router } from "@/constants";
+import { cls } from "@/libs/utils";
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 const Modal = () => {
+  const pathname = usePathname();
   return (
     <>
       <div className="fixed top-0 right-0 w-full h-screen" />
-      <div className="absolute h-auto px-2 py-2 space-y-10 bg-white border border-gray-300 rounded-md shadow-sm dark:border-zinc-500 dark:bg-zinc-700 top-14 w-36 right-4">
+      <div className="absolute h-auto px-2 py-2 space-y-10 bg-transparent border bg-[#202023] border-gray-300 rounded-md shadow-md dark:border-zinc-500 top-14 w-36 right-4">
         <div className="flex flex-col space-y-2">
-          <Link href="/" className="transition-colors hover:text-gray-400">
-            home
-          </Link>
-          <Link href="/about" className="transition-colors hover:text-gray-400">
-            about
-          </Link>
-          <Link href="/posts" className="transition-colors hover:text-gray-400">
-            posts
-          </Link>
-          <Link
-            href="/contact"
-            className="transition-colors hover:text-gray-400"
-          >
-            contact
-          </Link>
+          {router.map(([href, name]) => (
+            <Link
+              key={href}
+              className={cls(
+                "transition-colors",
+                pathname === href ? "text-indigo-400" : "hover:text-indigo-400"
+              )}
+              href={href}
+            >
+              {name}
+            </Link>
+          ))}
         </div>
       </div>
     </>
