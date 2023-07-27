5261583e50adcf5e1c01e2fe95bd474e7d0720d9
Telescope <ehwlgns000@gmail.com>
2023-07-27 18:07:45 +0900

    refactor: post detail refactor

--- src/app/posts/[slug]/PostContent.tsx
+++ src/app/posts/[slug]/PostContent.tsx
@@ -1,3 +1,4 @@
+import Divider from "@/components/Divider";
 import MarkdownViewer from "@/components/posts/MarkdownViewer";
 import { AiTwotoneCalendar } from "react-icons/ai";
 
@@ -16,15 +17,17 @@ export default function PostContent({
 }: IPostContent) {
   return (
     <section>
-      <div className="flex items-center justify-end space-x-3 font-semibold text-gray-500 dark:text-gray-300 ">
-        <AiTwotoneCalendar />
-        <p>{date.toString()}</p>
-      </div>
       <h1 className="text-4xl font-bold">{title}</h1>
-      <p className="mb-3 text-xl text-gray-600 dark:text-gray-300">
-        {description}
-      </p>
-      <div className="w-full bg-white border-b border-gray-600 dark:border-gray-300 " />
+      <div className="flex items-center justify-between w-full">
+        <p className="text-xl text-gray-600 dark:text-gray-300">
+          {description}
+        </p>
+        <div className="flex items-center justify-end space-x-3 font-semibold text-gray-500 dark:text-gray-300 ">
+          <AiTwotoneCalendar />
+          <p>{date.toString()}</p>
+        </div>
+      </div>
+      <Divider />
       <MarkdownViewer content={content} />
     </section>
   );
