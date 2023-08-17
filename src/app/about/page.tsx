bee277f8db0bddec0cc183f70d1c2871d20d574c
Telescope <ehwlgns000@gmail.com>
2023-08-17 15:21:34 +0900

    fix: about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,13 +19,12 @@ bg-slate-100 rounded-md"
       >
         <IntroduceForm
           title="Who am I"
-          description={"the world's greatest genius programmer".toUpperCase()}
-          image={ProfileImage}
+          description={["풀스택 개발자를 목표로 공부하고 있는 도지훈 입니다."]}
         />
-        <IntroduceForm title="Career" image={Nothing} imageHeight={32} />
+        <IntroduceForm title="Career" description={["강남대학교 소프트웨어공학부", "정보처리기사"]} />
         <IntroduceForm title="Skills">
           <div className="grid gap-8 px-2 py-4 md:grid-cols-3 sm:grid-cols-2 max-h-fit">
-            <div className="text-blue-600">
+            <div className="text-blue-400">
               <h1 className="mb-2 text-xl font-bold text-center">
                 약간의 복습 필요
               </h1>
@@ -35,7 +34,7 @@ bg-slate-100 rounded-md"
               <p>React Native</p>
               <p>Styled-Components</p>
             </div>
-            <div className="text-violet-500">
+            <div className="text-violet-400">
               <h1 className="mb-2 text-xl font-bold text-center">자주 사용</h1>
               <p>React</p>
               <p>Next.js</p>
@@ -43,7 +42,7 @@ bg-slate-100 rounded-md"
               <p>Prisma ORM</p>
               <p>Tailwind css</p>
             </div>
-            <div className="text-orange-600">
+            <div className="text-orange-400">
               <h1 className="mb-2 text-xl font-bold text-center">접해봄</h1>
               <p>MongoDB</p>
             </div>
