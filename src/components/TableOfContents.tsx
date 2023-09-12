c398c74cb0f6027ad735a79805180e311c496bdf
Telescope <ehwlgns000@gmail.com>
2023-09-12 17:59:16 +0900

    create: create Toc Mobile

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -2,7 +2,8 @@
 import useActiveToc from "@/hooks/useActiveToc";
 import { cls } from "@/libs/utils";
 import { useEffect, useState } from "react";
-
+import { RiMenuUnfoldLine, RiCloseLine } from "react-icons/ri";
+import { AnimatePresence, motion } from "framer-motion";
 interface IHeadings {
   text: string;
   level: string;
@@ -10,6 +11,7 @@ interface IHeadings {
 }
 const TableOfContents = () => {
   const [headings, setHeadings] = useState<IHeadings[]>();
+  const [open, setOpen] = useState(false);
   const { activeId } = useActiveToc();
 
   useEffect(() => {
@@ -25,39 +27,106 @@ const TableOfContents = () => {
     );
     setHeadings(elements);
   }, []);
+
+  useEffect(() => {
+    if (open) {
+      document.querySelector(`#list_${activeId}`)?.scrollIntoView({
+        block: "nearest",
+        behavior: "smooth",
+      });
+    }
+  }, [open]);
+
   return (
-    <section className="sticky hidden px-2 ml-3 text-sm top-32 xl:block">
-      <ul className="absolute space-y-2 -right-52">
-        {headings?.map((heading) => (
-          <li
-            key={heading.id}
-            className={`${heading.level === "3" && "ml-2"} ${
-              heading.level === "4" && "ml-6 list-inside list-disc"
-            }`}
-          >
-            <a
-              href={`#${heading.id}`}
-              className={cls(
-                "transition-colors hover:text-sky-500 dark:hover:text-violet-400",
-                activeId === heading.id
-                  ? "text-sky-500 dark:text-violet-400"
-                  : ""
-              )}
-              onClick={(e) => {
-                e.preventDefault();
-                document.querySelector(`#${heading.id}`)?.scrollIntoView({
-                  behavior: "smooth",
-                  block: "center",
-                  inline: "nearest",
-                });
-              }}
+    <>
+      <section className="sticky hidden px-2 ml-3 text-sm top-32 xl:block">
+        <ul className="absolute space-y-2 -right-52">
+          {headings?.map((heading) => (
+            <li
+              key={heading.id}
+              className={`${heading.level === "3" && "ml-2"} ${
+                heading.level === "4" && "ml-6 list-inside list-disc"
+              }`}
             >
-              {heading.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </section>
+              <a
+                href={`#${heading.id}`}
+                className={cls(
+                  "transition-colors hover:text-sky-500 dark:hover:text-violet-400",
+                  activeId === heading.id
+                    ? "text-sky-500 dark:text-violet-400"
+                    : ""
+                )}
+                onClick={(e) => {
+                  e.preventDefault();
+                  document.querySelector(`#${heading.id}`)?.scrollIntoView({
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "nearest",
+                  });
+                }}
+              >
+                {heading.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section className="sticky right-0 block text-sm top-32 xl:hidden">
+        {open && (
+          <div
+            onClick={() => setOpen((prev) => !prev)}
+            className="fixed top-0 bottom-0 left-0 right-0 bg-black opacity-20"
+          />
+        )}
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className="absolute right-0 px-3 py-3 text-white transition-colors rounded-full bg-sky-400 hover:bg-sky-500 focus:bg-sky-500 dark:hover:bg-violet-500 dark:focus:bg-violet-500 active:shadow-inner dark:bg-violet-400"
+        >
+          {open ? <RiCloseLine /> : <RiMenuUnfoldLine />}
+        </button>
+        <AnimatePresence initial={false} mode="wait">
+          {open ? (
+            <motion.ul
+              initial={{ x: 100 }}
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: 100, opacity: 0 }}
+              transition={{ duration: 0.2, type: "spring" }}
+              className="absolute border dark:border-gray-500 space-y-2 overflow-auto dark:bg-[#202023] bg-white right-0 h-48 px-2 py-2 rounded-md shadow-md top-12 w-52"
+            >
+              {headings?.map((heading) => (
+                <li
+                  key={heading.id}
+                  className={`${heading.level === "3" && "ml-2"} ${
+                    heading.level === "4" && "ml-6 list-inside list-disc"
+                  }`}
+                >
+                  <a
+                    id={`list_${heading.id}`}
+                    href={`#${heading.id}`}
+                    className={cls(
+                      "transition-colors hover:text-sky-500 dark:hover:text-violet-400",
+                      activeId === heading.id
+                        ? "text-sky-500 dark:text-violet-400"
+                        : ""
+                    )}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      document.querySelector(`#${heading.id}`)?.scrollIntoView({
+                        behavior: "smooth",
+                        block: "center",
+                        inline: "nearest",
+                      });
+                    }}
+                  >
+                    {heading.text}
+                  </a>
+                </li>
+              ))}
+            </motion.ul>
+          ) : null}
+        </AnimatePresence>
+      </section>
+    </>
   );
 };
 
