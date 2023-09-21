93dec37dcef54d57127d6571f4300602cde336cd
Telescope <ehwlgns000@gmail.com>
2023-09-21 14:16:58 +0900

    refactor: edit ToC Title style

--- src/components/TableOfContents/DesktopToC.tsx
+++ src/components/TableOfContents/DesktopToC.tsx
@@ -6,7 +6,9 @@ const DeskTopToC = ({ onClick, headings, activeId }: ToCProps) => {
   return (
     <section className="sticky hidden px-2 ml-3 text-sm top-32 xl:block">
       <nav className="absolute overflow-auto -right-52">
-        <h1 className="py-2 my-2 text-lg border-b">Table Of Contents</h1>
+        <h1 className="py-2 my-2 text-lg font-semibold border-b">
+          Table Of Contents
+        </h1>
         <ul className="max-h-full pb-2 space-y-2 ">
           {headings?.map((heading) => (
             <li
