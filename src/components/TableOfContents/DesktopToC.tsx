39a76924790ea81b1a37159bb9722298d04a687c
Jihun Do <77131309+DoDevet@users.noreply.github.com>
2023-09-21 14:19:08 +0900

    ToC 타이틀 추가 (#3)
    * test: Branch Test
    
    * refactor: add Table of contents Title
    
    * refactor: edit ToC Title style

--- src/components/TableOfContents/DesktopToC.tsx
+++ src/components/TableOfContents/DesktopToC.tsx
@@ -5,29 +5,34 @@ import { ToCProps } from ".";
 const DeskTopToC = ({ onClick, headings, activeId }: ToCProps) => {
   return (
     <section className="sticky hidden px-2 ml-3 text-sm top-32 xl:block">
-      <ul className="absolute space-y-2 -right-52">
-        {headings?.map((heading) => (
-          <li
-            key={heading.id}
-            className={`${heading.level === "3" && "ml-2"} ${
-              heading.level === "4" && "ml-6 list-inside list-disc"
-            }`}
-          >
-            <a
-              href={`#${heading.id}`}
-              className={cls(
-                "transition-colors hover:text-sky-500 dark:hover:text-violet-400",
-                activeId === heading.id
-                  ? "text-sky-500 dark:text-violet-400"
-                  : ""
-              )}
-              onClick={(e) => onClick(e, heading.id)}
+      <nav className="absolute overflow-auto -right-52">
+        <h1 className="py-2 my-2 text-lg font-semibold border-b">
+          Table Of Contents
+        </h1>
+        <ul className="max-h-full pb-2 space-y-2 ">
+          {headings?.map((heading) => (
+            <li
+              key={heading.id}
+              className={`${heading.level === "3" && "ml-2"} ${
+                heading.level === "4" && "ml-6 list-inside list-disc"
+              }`}
             >
-              {heading.text}
-            </a>
-          </li>
-        ))}
-      </ul>
+              <a
+                href={`#${heading.id}`}
+                className={cls(
+                  "transition-colors hover:text-sky-500 dark:hover:text-violet-400",
+                  activeId === heading.id
+                    ? "text-sky-500 dark:text-violet-400"
+                    : ""
+                )}
+                onClick={(e) => onClick(e, heading.id)}
+              >
+                {heading.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </section>
   );
 };
